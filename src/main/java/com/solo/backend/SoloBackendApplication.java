1923f2589578a3be8439d8075fba5d1f65ac059a
Marco Ames <marco.ames@edu.pucrs.br>
2024-11-22 09:30:28 -0300

    initial commit

---
+++ src/main/java/com/solo/backend/SoloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.solo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solo/backend/SoloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
